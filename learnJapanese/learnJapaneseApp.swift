42959dce9f5fd9a0aab18e9b00f3b100f32c5402
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-11-27 13:15:25 +0900

    Initial Commit

---
+++ learnJapanese/learnJapaneseApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnJapaneseApp.swift
+//  learnJapanese
+//
+//  Created by 최동현 on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct learnJapaneseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
